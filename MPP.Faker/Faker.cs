353fe357236eaa16a0cfd2230978943a68b482fb
Shampyn <31312215+Shampyn@users.noreply.github.com>
2019-11-13 15:20:17 +0300

    simple logic

--- MPP.Faker/Faker.cs
+++ MPP.Faker/Faker.cs
@@ -30,31 +30,95 @@ namespace MPP.Faker
             Generators.Add(typeof(char), new CharGenerator());
             listGenerator = new ListGenerator(Generators);
         }
-
+      
         public T Create<T>()
+            {
+                Type type = typeof(T);
+                if (type.IsAbstract || type.IsInterface || type == typeof(void))
+                {
+                    return default(T);
+                }
+                if (type.IsGenericType)
+                {
+                    return (T)listGenerator.Generate((Type)type.GenericTypeArguments.GetValue(0));
+                }
+                IGenerator value;
+                Generators.TryGetValue(type, out value);
+                if (value != null)
+                {
+                    return (T)value.Generate();
+                }
+
+                if (!type.IsAbstract || !type.IsPrimitive)
+                {
+                    ConstructorInfo ConstructorWithMaxArgs = GetConstructorWithMaxParams(type);
+                    var instance = GenerateObjectFromConstructor(ConstructorWithMaxArgs);
+                    return (T)GenerateFieldsAndProperties(type, instance);
+                }
+                return default(T);
+            }
+
+        private ConstructorInfo GetConstructorWithMaxParams(Type type)
         {
-            Type type = typeof(T);
-            if (type.IsAbstract || type.IsInterface || type == typeof(void))
+            ConstructorInfo constructorwithmaxparams = null;
+            int count = 0;
+            foreach (ConstructorInfo constructor in type.GetConstructors())
             {
-                return default(T);
+                if (count < constructor.GetParameters().Count())
+                {
+                    constructorwithmaxparams = constructor;
+                    count = constructor.GetParameters().Count();
+                }
             }
-            if (type.IsGenericType)
-            {
-                return (T)listGenerator.Generate((Type)type.GenericTypeArguments.GetValue(0));
-            }
-            IGenerator value;
-            Generators.TryGetValue(type, out value);
-            if (value != null)
-            {
-                return (T)value.Generate();
-            }
-            else
-            {
-                return default(T);
-            }
+            return constructorwithmaxparams;
         }
 
+        private object GenerateObjectFromConstructor(ConstructorInfo constructor)
+        {
+            ParameterInfo[] parameters = constructor.GetParameters();
+            object[] parametersValues = new object[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                if (parameters[i].ParameterType.IsGenericType)
+                {
+                    parametersValues[i] = listGenerator.Generate((Type)parameters[i].ParameterType.GenericTypeArguments.GetValue(0));
+                }
+                else
+                {
+                    IGenerator valueGenerator;
+                    Generators.TryGetValue(parameters[i].ParameterType, out valueGenerator);
+                    parametersValues[i] = valueGenerator.Generate();
+                }
+            }
+            return constructor.Invoke(parametersValues);
+        }
 
+        private object GenerateFieldsAndProperties(Type type, object instance)
+        {
+            FieldInfo[] fields = type.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                IGenerator value;
+                Generators.TryGetValue(field.FieldType, out value);
+                field.SetValue(instance, value.Generate());
+            }
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (property.PropertyType.IsGenericType) 
+                {
+                    property.SetValue(instance, listGenerator.Generate((Type)property.PropertyType.GenericTypeArguments.GetValue(0)));                  
+                }
+                else
+                {
+                    IGenerator value;
+                    Generators.TryGetValue(property.PropertyType, out value);
+                    property.SetValue(instance, value.Generate());
+                }               
+            }
+            return instance;
+        }
     }
 }
+
 
--- MPP.Faker/Program.cs
+++ MPP.Faker/Program.cs
@@ -6,6 +6,51 @@ using System.Threading.Tasks;
 
 namespace MPP.Faker
 {
+    public class A
+    {
+        int c;
+        bool k;
+        List<byte> T;
+        public int testint { get; set; }
+    }
+
+    public class B
+    {
+        public C c { get; set; }
+    }
+
+    public class C
+    {
+        public B b { get; set; }
+    }
+
+    public class D
+    {
+        public  int a;
+        public int b;
+        public char k;
+        public string Name { get; set; }
+        public  bool c;
+        public List<bool> BoolList;
+        public D()
+        {
+            a = 10;
+            b = 2;
+        }
+        public D(int a, int b, bool c, List<bool> listbool)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            BoolList = listbool;
+        }
+        public D(int k, int e)
+        {
+            a = e;
+            b = k;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -27,6 +72,10 @@ namespace MPP.Faker
             {
                 Console.WriteLine("{0}", listtest.ElementAt(i));
             }
+
+            D classtest = faker.Create<D>();
+
+            Console.WriteLine("{0}, {1},{2}", classtest.k, classtest.a, classtest.b);
             Console.ReadLine();
 
         }
